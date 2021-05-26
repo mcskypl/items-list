b78bd990f7f18b185b39ad51bb9bd5a7c19cd591
Sebastian Westfal <mcskypl@gmail.com>
2021-05-26 22:36:11 +0200

    Update DataList.js

--- src/components/ItemsList/DataList/DataList.js
+++ src/components/ItemsList/DataList/DataList.js
@@ -2,8 +2,8 @@ import React from 'react';
 import style from './DataList.module.scss';
 
 const DataList = ({ id, statusEditFn }) => (
-  <select value="Zwolnione" className={style.select} id={id} onChange={statusEditFn}>
-    <option selected>Wybierz...</option>
+  <select className={style.select} id={id} onChange={statusEditFn}>
+    <option selected disabled>--Wybierz--</option>
     <option value="Zwolnione">Zwolnione</option>
     <option value="Wstrzymane">Wstrzymane</option>
     <option value="Niezgodne">Niezgodne</option>
